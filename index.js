16a886713f13b250e057a0bd2ec17e9289810061
ahmed.zaky030 <azm030@gmail.com>
2017-09-18 14:35:57 +0200

    index and homeRouter has been linting

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ var servStatic = require('serve-static');
 var routes = require('./routes/homeRouter');
 var cors = require('cors');
 var bodyparser = require('body-parser');
-
+const dirname = path.dirname;
 
 app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
 
-app.use(servStatic(path.join(__dirname,'public')));
-app.use(servStatic(path.join(__dirname,'views')));
-app.set('views' , path.join(__dirname,'views'));
+app.use(servStatic(path.join(dirname,'public')));
+app.use(servStatic(path.join(dirname,'views')));
+app.set('views' , path.join(dirname,'views'));
 app.set('view engine','ejs');
 
 app.use('/s' , routes); 
 
-app.use('/' , function(req , res , next){
-    res.render("index");    
+app.use('/' , function(req , res){
+	res.render('index');    
 });
 
 app.listen(3000 , function(){
-    console.log('Server is working on 3000');
+	// console.log('Server is working on 3000');
 });
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,46 +1,46 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var studentCon = require('../controller/studentController');
+var studentCon = require("../controller/studentController");
 
 
 var results ;
-router.get('/', function(req, res, next){
-    //console.log('inside the router');
-    studentCon.getData(null , function(data){
-        //console.log('in callback');
-        results = data;        
-        //res.json(results);
-        res.render('karma');
-    });
+router.get("/", function(req, res){
+	//console.log('inside the router');
+	studentCon.getData(null , function(data){
+		//console.log('in callback');
+		results = data;        
+		//res.json(results);
+		res.render("krma");
+	});
        
-}).post('/', function (req, res) {
+}).post("/", function (req, res) {
     
-    var name = req.body.name;
-    var age = +req.body.age;
-    var lang = req.body.lang;
-    var params  = {'name':name , 'age':age , 'lang':lang};
-    var result = studentCon.addStudent(params , function(result){
-        console.log('result post in router');
-        console.log(result);
-        res.json(result);
-    });
+	var name = req.body.name;
+	var age = +req.body.age;
+	var lang = req.body.lang;
+	var params  = {"name":name , "age":age , "lang":lang};
+	studentCon.addStudent(params , function(result){
+		//console.log('result post in router');
+		//console.log(result);
+		res.json(result);
+	});
 })
 
 
-.get('/age/:age/lang/:lang', function(req, res, next){
-  var params ={
-      age: +req.params.age,
-      lang: req.params.lang !='null' ? req.params.lang : null
-  };
+	.get("/age/:age/lang/:lang", function(req, res){
+		var params ={
+			age: +req.params.age,
+			lang: req.params.lang !="null" ? req.params.lang : null
+		};
 
-  studentCon.getData(params, function(data){
-    //console.log('in callback');
-    results = data;        
-    res.json(results);
-    //res.render('karma');
+		studentCon.getData(params, function(data){
+			//console.log('in callback');
+			results = data;        
+			res.json(results);
+			//res.render('karma');
 
     
-}); 
-})
+		}); 
+	});
 
 module.exports = router;
